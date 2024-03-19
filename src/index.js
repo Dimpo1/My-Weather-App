49c23e28879f603239b5bfe8983432f797a80b92
Dimpo1 <111021090+Dimpo1@users.noreply.github.com>
2024-03-19 06:10:39 +0200

    Api search functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function changeValues(response) {
+  let temperatureElement = document.querySelector("#temp");
+  currentTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let Key = "te015e3a1bdea7604234a21223o5f7af";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${Key}`;
+  axios.get(apiUrl).then(changeValues);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let citySearchInput = document.querySelector("#citySearch");
-  let cityElement = document.querySelector("#myCity");
-  console.log(cityElement);
-  cityElement.innerHTML = citySearchInput.value;
+
+  searchCity(citySearchInput.value);
 }
 let searchElement = document.querySelector("#searchForm");
 searchElement.addEventListener("submit", searchSubmit);
+
+searchCity("Johannesburg");
